592afe9c7229e4446c60a4fcd871dd1d50465077
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2025-01-27 13:01:50 +0530

    27/01/2025
    add screen

---
+++ BookMyShow-main/BookMyShow/BookMyShow.java
@@ -0,0 +1,27 @@
+package BookMyShow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BookMyShow {
+    private static ArrayList<User> userArrayList=new ArrayList<>();
+    private static ArrayList<Admin> adminArrayList=new ArrayList<>();
+    private static ArrayList<String> locationArrayList=new ArrayList<>();
+    private static HashMap<String,Theatre> theatreHashMap=new HashMap<>();
+    public static ArrayList<User> getUserArrayList(){
+        return userArrayList;
+    }
+    public static ArrayList<Admin> getAdminArrayList(){
+        return adminArrayList;
+    }
+
+
+    public static ArrayList<String> getLocationArrayList() {
+        return locationArrayList;
+    }
+
+
+    public static HashMap<String, Theatre> getTheatreHashMap() {
+        return theatreHashMap;
+    }
+}
